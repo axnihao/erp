bc04c486e8098d6f7897d3ae3fece02eaea5751c
zixingc <974212451@qq.com>
2019-04-01 12:25:12 +0800

    modified hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,8 @@ public class Hello
 	public static void main(String arg[])
 	{
 		System.out.println("Hello Git!!!");
-		
+		System.out.println("Hello ZXC");
+		System.out.println("Hello World!!!");
 	}
 	
 }
